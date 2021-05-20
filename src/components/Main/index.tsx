ba32969a0f47eca204dd497acb183279c53d3acb
David Clausen <davidclausen2051@gmail.com>
2021-05-19 19:36:29 -0500

    fix: remove onclick

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,8 @@
-import { changeLocation } from '../../utils/routing';
 import { Card } from '../Card';
 
 export default function Main() {
   return (
-    <main
-      className="main"
-      role="presentation"
-      onClick={() => {
-        const url = window.location.pathname === '/about' ? '/' : '/about';
-        changeLocation(url);
-      }}
-    >
+    <main className="main" role="presentation">
       <Card />
     </main>
   );
